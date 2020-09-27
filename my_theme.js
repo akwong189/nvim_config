9ec53413d646f1a63f5c0b9709f4f37cb0445fb1
Adley Wong <adleywong54@gmail.com>
2020-09-27 14:00:25 -0700

    My theme color

---
+++ my_theme.js
@@ -0,0 +1,28 @@
+black       = '#24292e';
+red         = '#f97583'; // red
+green       = '#85e89d'; // green
+yellow      = '#ffea7f'; // yellow
+blue        = '#79b8ff'; // blue
+magenta     = '#b392f0'; // pink
+cyan        = '#73e3ff'; // cyan
+white       = '#f6f8fa'; // light gray
+lightBlack  = '#959da5'; // medium gray
+lightRed    = '#f97583'; // red
+lightGreen  = '#85e89d'; // green
+lightYellow = '#ffea7f'; // yellow
+lightBlue   = '#79b8ff'; // blue
+lightMagenta= '#b392f0'; // pink
+lightCyan   = '#73e3ff'; // cyan
+lightWhite  = '#f6f8fa'; // white
+
+t.prefs_.set('color-palette-overrides',
+                 [ black , red     , green  , yellow,
+                  blue     , magenta , cyan   , white,
+                  lightBlack   , lightRed  , lightGreen , lightYellow,
+                  lightBlue    , lightMagenta  , lightCyan  , lightWhite ]);
+
+t.prefs_.set('cursor-color', '#24292e');
+t.prefs_.set('cursor-blink', true);
+
+t.prefs_.set('foreground-color', '#f6f8fa');
+t.prefs_.set('background-color', '24292e');
